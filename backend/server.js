ee1617abe8a46fe94149e0acdb1025091ac689fb
mayheart0505 <54180147+mayheart0505@users.noreply.github.com>
2025-11-02 15:38:50 -0500

    Add files via upload
    Moved server.js file outside so easier access to package.json, also has added car routes

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+dotenv.config();  
+
+import express from "express";
+import authRoutes from "./src/routes/auth-route.js";
+import carRoutes from "./src/routes/carRoutes.js";
+import { connectDB } from "./src/config/db.js";
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+// Handle CORS properly
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/car", carRoutes);
+
+app.get("/test", (req, res) => {
+  res.json({ message: "Server is running" });
+});
+
+const port = process.env.PORT || 5001;
+app.listen(port, () => {
+  console.log("Server started on PORT:", port);
+});
